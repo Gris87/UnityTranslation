b71f8affd6731a9dc2f227166490fffa2ff38b5e
Gris87 <Gris87@yandex.ru>
2015-01-29 23:11:38 +0300

    XSLT template

--- Build/generate_docs.bat
+++ Build/generate_docs.bat
@@ -12,6 +12,7 @@ set MCS=%UNITY_PATH%\Editor\Data\MonoBleedingEdge\bin\mcs.bat
 set MDOC=%ORIGINAL_PATH%\mdoc-net-2010-01-04\mdoc
 set LIBS_DIR=%UNITY_PATH%\Editor\Data\Managed
 set EXT_LIBS_DIR=%UNITY_PATH%\Editor\Data\UnityExtensions\Unity\GUISystem\4.6.1
+set XSLT_TEMPLATE=%ORIGINAL_PATH%\doctemplate.xsl
 
 echo ---------------------------------------------------------------
 echo Path to mcs  = %MCS%
@@ -28,7 +29,7 @@ rmdir /S /Q docs
 
 call %MCS% -lib:%LIBS_DIR% -lib:%EXT_LIBS_DIR% -r:UnityEngine -r:UnityEngine.UI /doc:doc.xml -target:library -out:lib.dll *.cs Generated\*.cs Generated\UI\*.cs
 call %MDOC% update --lib:%LIBS_DIR% --lib:%EXT_LIBS_DIR% -r:UnityEngine -r:UnityEngine.UI -i doc.xml -o doc_stubs lib.dll
-call %MDOC% export-html -o docs doc_stubs
+call %MDOC% export-html --template %XSLT_TEMPLATE% -o docs doc_stubs
 
 rmdir /S /Q doc_stubs
 del doc.xml
