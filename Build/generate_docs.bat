bd94551bc06d49366bcf1e2e3d851b4d5892915c
Gris87 <Gris87@yandex.ru>
2015-02-19 23:18:13 +0300

    PDF documentation

--- Build/generate_docs.bat
+++ Build/generate_docs.bat
@@ -5,10 +5,20 @@ set ORIGINAL_PATH=%CD%
 rem ------------------------------------------------
 
 rmdir /S /Q ..\Development\Assets\Scripts\UnityTranslation\.docs
+rmdir /S /Q ..\Development\Assets\Scripts\UnityTranslation\.latex
 
 cd ForDoxygen
 
 doxygen
+
+rem ------------------------------------------------
+
+cd ..\..\Development\Assets\Scripts\UnityTranslation\.latex
+call make.bat
+
+cd ..
+move /Y .latex\refman.pdf .docs.pdf
+rmdir /S /Q .latex
 
 rem ------------------------------------------------
 
